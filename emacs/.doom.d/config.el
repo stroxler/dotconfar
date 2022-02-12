1445a9cbeb2d6063c461aa2b41d617c2b47051f3
Steven Troxler <steven.troxler@gmail.com>
2022-02-12 09:54:04 -0800

    Make doom org + theme env-var-configurable

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,13 +1,30 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+;; Helper functions for controlling emacs via environment variables
+(defun env-with-default-str (env-var default-value)
+  (let ((value (getenv env-var)))
+    (or value default-value)))
 
+(defun env-with-default-sym (env-var default-value)
+  (intern (env-with-default-str env-var (symbol-name default-value))))
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; Identify myself (meh, not at this time)
 (setq user-full-name "John Doe"
       user-mail-address "john@doe.com")
+
+;; Look and feel
+(setq doom-theme (env-with-default-sym "EMACS_THEME" 'doom-zenburn))
+(setq display-line-numbers-type t)
+
+;; Make evil treat '_' the way vim does
+(modify-syntax-entry ?_ "w")
+
+;; Org configuration
+(setq org-directory (env-with-default-str "ORG_DIRECTORY" "~/OrgFiles"))
+(setq org-roam-directory (env-with-default-str "ORG_ROAM_DIRECTORY" "~/OrgRoamFiles"))
+
+;; More config options, these come from the doom init output
+;; ---------------------------------------------------------
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -21,21 +38,6 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-zenburn)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/OrgFiles")
-(setq org-roam-directory "~/OrgRoamFiles")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
@@ -55,9 +57,3 @@
 ;; they are implemented.
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; TROX-SPECIFIC-CHANGES ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Make evil treat _ the way vim does.
-(modify-syntax-entry ?_ "w")
