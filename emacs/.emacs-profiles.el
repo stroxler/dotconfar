d5010fb2c8411dee12d31edf5e8a6fb010f59aaf
Steven Troxler <steven.troxler@gmail.com>
2023-04-24 18:40:48 -0700

    Add doom to chemacs2 profiles :)

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,2 +1,5 @@
 (("runemacs" . ((user-emacs-directory . "~/runemacs-emacs.d")))
- ("spacemacs" . ((user-emacs-directory . "~/spacemacs-emacs.d"))))
+ ("spacemacs" . ((user-emacs-directory . "~/spacemacs-emacs.d")))
+ ("doom" . ((user-emacs-directory . "~/doom-emacs.d")
+           (env . (("DOOMDIR" . "~/.doom.d")))))
+)
