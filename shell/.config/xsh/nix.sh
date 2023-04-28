c8e63aa3588ebb1d0846349205f57755bbcd71ed
Steven Troxler <steven.troxler@gmail.com>
2023-04-28 15:40:06 -0700

    Tweaks to nix + starship

--- shell/.config/xsh/nix.sh
+++ shell/.config/xsh/nix.sh
@@ -1,8 +1,1 @@
-
-
-
-if [[ -z "$(which nix)" ]]; then
-	if [ -e /home/stroxler/.nix-profile/etc/profile.d/nix.sh ]; then
-		source /home/stroxler/.nix-profile/etc/profile.d/nix.sh;
-	fi
-fi
+export PATH=$PATH:"${HOME}/.nix-profile/bin"
