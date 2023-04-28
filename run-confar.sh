0b601db2c003e389758a11c8651e85b2f78d3ace
Steven Troxler <steven.troxler@gmail.com>
2023-04-28 13:43:36 -0700

    Move bash directory to shell, for hybrid setup
    Summary:
    
    I want to start moving toward a setup that will tolerate me working
    in multiple shells (in particular both bash and zsh). To that end,
    I want to call the section of my dotconfar `shell` rather than `bash`.
    
    The first thing I want to add to it is my starship.rs config.

--- run-confar.sh
+++ run-confar.sh
@@ -1,5 +1,5 @@
 set -ex
 
 cd ~ && confar-v0 -s ~/dotconfar/emacs -d ~
-cd ~ && confar-v0 -s ~/dotconfar/bash -d ~
+cd ~ && confar-v0 -s ~/dotconfar/shell -d ~
 cd ~ && confar-v0 -s ~/dotconfar/misc-tools -d ~
